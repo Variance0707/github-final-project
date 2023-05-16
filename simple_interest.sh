c65cfd356c7a9e2983f32e049bbf94fb1bad57fc
Variance <124952475+Variance0707@users.noreply.github.com>
2023-05-16 17:56:37 +0900

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,8 +1,28 @@
-#! /bin/bash
- i = 0
- j = 10
- while [$i -lt $j]
-do
-  echo "num1 = $i,num2 = $j"
-  ((i++))
-done
+#!/bin/bash
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+
+   # Do not use this in production. Sample purpose only.
+
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # jcmbu
+
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+
+   # Output:
+   # simple interest = p*t*r
+
+   echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+
+   s=`expr $p \* $t \* $r / 100`
+   echo "The simple interest is: "
+   echo $s
